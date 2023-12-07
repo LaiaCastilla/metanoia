bfc5a8f85687a7bc444ead3f44b0ebac995a18d8
Laia Castilla <lcastillaiamoros@gmail.com>
2023-12-07 14:35:40 +0100

    the same

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavigationBar from "./components/NavigationBar";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <NavigationBar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
