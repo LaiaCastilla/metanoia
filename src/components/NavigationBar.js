d34435c1dac578702f9e4571946c7065d1a701e7
Laia Castilla <lcastillaiamoros@gmail.com>
2023-12-07 10:41:28 +0100

    navigation bar established

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,27 +3,34 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../logo.svg";
 
 function NavigationBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            src={logo}
+            alt="Metanoia logo"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Metanoia Therapy
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+          <Nav className="ms-auto">
+            <Nav.Link href="#home" className="active">
+              Accueil
+            </Nav.Link>
+            <Nav.Link href="#about">À propos</Nav.Link>
+            <NavDropdown title="Soins" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">
+                Hypnothérapie
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Reiki</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
