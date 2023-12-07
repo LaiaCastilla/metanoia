72620ab4d312d44d90caad37a52712d710ae4ff1
Laia Castilla <lcastillaiamoros@gmail.com>
2023-12-07 11:00:01 +0100

    installing base css

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <NavigationBar />
+      <body>
+        <NavigationBar />
+      </body>
     </div>
   );
 }
