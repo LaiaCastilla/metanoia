e95b1f73eb131529a19f6f3c6572506dbafa518e
Laia Castilla <lcastillaiamoros@gmail.com>
2023-12-11 17:48:25 +0100

    manage to put text in citation

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,26 @@
 import React from "react";
 import olive from "../img/decor/olive.svg";
+import "../styles/stylesComponents/Contact.scss";
 
 function Contact() {
   return (
     <div className="contact">
       <div className="citation">
-        <img src={olive} alt="olive" className="citation__olive" width="100%" />
+        {/* <figure className="image position-relative"> */}
+        <img
+          src={olive}
+          alt="olive"
+          className="citation__olive img-fluid"
+          width="100%"
+        />
+        <div className="citation__text">
+          “Vous êtes le plus beau projet sur lequel vous pouvez travailler.”
+        </div>
+        {/* put this text over the image */}
+        {/* <figcaption className="citation__text">
+            “Vous êtes le plus beau projet sur lequel vous pouvez travailler.”
+          </figcaption> */}
+        {/* </figure> */}
       </div>
     </div>
   );
