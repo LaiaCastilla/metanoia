96a1e25f89f2331a1f6f2b6f8e63bcfd0b217aa7
Laia Castilla <lcastillaiamoros@gmail.com>
2023-12-12 12:35:16 +0100

    paragraph is added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -42,7 +42,39 @@ function Contact() {
         <div className="contact__content container-fluid">
           <h2 className="contact__content--title">Hello et bienvenue</h2>
           <div className="contact__content--text">
-            <img src={audrey} alt="audrey" />
+            <img
+              src={audrey}
+              alt="audrey"
+              className="contact__content--text-Img"
+            />
+            <div className="contact__content--text-paragraph">
+              Que vous dire sur moi ? Je m’appelle Audrey et que j’ai 31 ans.
+              Cela peut paraitre bien jeune, mais croyez-moi mon âme n’a pas
+              choisi la facilité dans cette nouvelle incarnation ! ce sont des
+              années marquées par les leçons de vie qui m’ont fait grandir à
+              vitesse grand V et m’ont révélées déjà très tôt mon chemin de vie.
+              <br /> <br />
+              Car oui, mon envie d’aider les autres ne date pas d’hier. Déjà
+              enfant, je rêvais de devenir médecin pour soigner « tous les bobos
+              du monde ». Seulement voilà, l’Univers a dû sentir que la médecine
+              occidentale moderne n’était pas vraiment faite pour moi et que je
+              n’étais pas encore prête pour cette voie.
+              <br /> <br /> Alors me voilà emportée par le flot de la vie dans
+              des études de commerce, qui ont tout de même veillées à ce que je
+              ne m’éloigne pas trop de ma voie, avec des postes dans des
+              services publics et des structures sociales et de soins. C’est là,
+              au contacts des autres, que mon empathie bienveillante, ma
+              capacité d’écoute et mon souhait d’apporter mon aide m’ont donné
+              l’envie de faire plus. <br /> <br />
+              C’est enfin en 2023 que le déclic se fait, je ne suis pas assez
+              épanouie, pas assez en accord avec moi-même… et me voilà lancée en
+              parallèle sur une formation de praticienne en Reiki Usui et
+              d’hypnothérapeute en hypnose Ericksonienne, car pour moi on ne
+              peut traiter sans prendre en compte la vue d’ensemble dans une
+              démarche holistique. <br /> <br /> C’est ainsi que Metanoia
+              Therapy est née et que nous allons ensemble écrire la suite de
+              l’histoire…
+            </div>
           </div>
         </div>
       </div>
