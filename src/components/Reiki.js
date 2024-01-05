355e8b1fe8d542d133d6afe384f70cbcd77bd258
Laia Castilla <lcastillaiamoros@gmail.com>
2024-01-05 15:18:23 +0100

    functional carousel

--- src/components/Reiki.js
+++ src/components/Reiki.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import CarouselItem from "react-bootstrap/CarouselItem";
+
 import circle from "../img/decor/HypnoElipse.svg";
 import leaf1 from "../img/decor/ReikiLeaf1.svg";
 import leaf2 from "../img/decor/ReikiLeaf2.svg";
@@ -10,10 +13,68 @@ function Reiki() {
   return (
     <div className="Reiki">
       <div className="container-fluid">
-        <Image src={circle} alt="circle" className="Reiki__circle" fluid />
         <Image src={leaf1} alt="leaf" className="Reiki__leaf1" fluid />
         <Image src={leaf2} alt="leaf" className="Reiki__leaf2" fluid />
         <h2 className="Reiki__title">Le Reiki</h2>
+        <div className="Reiki__intro">
+          <div className="Reiki__intro--text">
+            <p className="Reiki__text">
+              Le Reiki est un soin énergétique japonais qui peut se pratiquer
+              par apposition des mains sur le corps ou à distance. Il se
+              pratique aussi bien sur les humains que sur les animaux. Cette
+              technique de soin a été créée par Mikao Usui, un moine bouddhiste
+              japonais né en 1865. De ce que l’on connait de lui, c'est au début
+              des années 1920 qu'il fonda son centre de guérison et forma 17
+              maitres qui donnèrent naissance à plusieurs lignées Reiki. <br />
+              <br />
+              Le Reiki Usui que nous connaissons en Occident, provient de
+              Chijuro Hayashi (élève de Mikao Usui) qui forma à son tour Hawayo
+              Takata, qui fut à l'origine de sa large diffusion en Occident.{" "}
+              <br />
+              <br />
+              C'est lors d'une retraite méditative que Mikao Usui eut une
+              révélation sur sa capacité à prodiguer des soins et sur le pouvoir
+              guérisseur de l'énergie universelle. Mais l'énergie qu'est-ce que
+              c'est ? <br />
+              <br />
+              Tout ce qui existe est énergie, la terre, l'eau, les êtres
+              vivants, l'univers. La matière n'est ni plus ni moins que de
+              l'énergie vibrant à une certaine fréquence. Le Reiki aide à
+              rééquilibrer cette fréquence énergétique pour nous apporter un
+              meilleur équilibre qui favorisera notre processus de guérison.
+            </p>
+            <h3 className="Reiki__subtitle">Pour...</h3>
+            <p className="Reiki__text">
+              Cette pratique est utilisée aussi bien pour réduire et lutter
+              contre le stress, pour le traitement des traumatismes, pour la
+              gestion des émotions, pour la confiance en soi ou de l'estime de
+              soi, ou encore pour surmonter ses peurs et croyances limitantes.
+            </p>
+          </div>
+          <div className="Reiki__usui">
+            <Image src={circle} alt="circle" className="Reiki__circle" fluid />
+            <div className="Reiki__usui--content">
+              <div className="Reiki__usui--title">Juste pour aujourd'hui,</div>
+              <div className="Reiki__usui--carousel">
+                <Carousel controls={false} indicators={false} pause="hover">
+                  <CarouselItem interval={2000}>
+                    libère-toi de toute colère
+                  </CarouselItem>
+                  <CarouselItem interval={2000}>
+                    libère-toi de tout souci
+                  </CarouselItem>
+                  <CarouselItem interval={2000}>
+                    fais preuve de gratitude
+                  </CarouselItem>
+                  <CarouselItem interval={2000}>agis de ton mieux</CarouselItem>
+                  <CarouselItem interval={2000}>
+                    sois bon envers toi-même et envers autrui
+                  </CarouselItem>
+                </Carousel>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
